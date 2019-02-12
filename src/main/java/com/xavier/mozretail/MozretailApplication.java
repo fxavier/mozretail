0ccfae96918f39d595af031e84c56ff42abedab8
Xavier Nhagumbe <xavierfrancisco353@gmail.com>
2019-02-12 10:31:19 +0200

    First commit

---
+++ src/main/java/com/xavier/mozretail/MozretailApplication.java
@@ -0,0 +1,14 @@
+package com.xavier.mozretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MozretailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MozretailApplication.class, args);
+	}
+
+}
+
